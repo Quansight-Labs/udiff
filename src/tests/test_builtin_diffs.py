5c70b2d6c6816c460bfc5f4b36b644a830001aeb
ethan <32364921+sangyx@users.noreply.github.com>
2020-05-26 17:57:15 +0800

    Use numerical differentiation to check derivatives (#16)
    * Use numerical differentiation to check derivatives
    
    * Add numerical differentiation

--- src/tests/test_builtin_diffs.py
+++ src/tests/test_builtin_diffs.py
@@ -6,7 +6,7 @@ import dask.array as da
 import udiff
 import sparse
 from math import *
-from random import uniform
+from random import uniform, randrange
 import unumpy.numpy_backend as NumpyBackend
 
 import unumpy.torch_backend as TorchBackend
@@ -76,19 +76,42 @@ EXCEPTIONS = {
     (DaskBackend, np.searchsorted),
 }
 
-
 @pytest.fixture(scope="session", params=LIST_BACKENDS)
 def backend(request):
     backend = request.param
     return backend
 
-def generate_test_data(n_elements=10, a=None, b=None):
+def generate_test_data(n_elements=12, a=None, b=None):
     if a is None:
         a = -10
     if b is None:
         b = 10
     x_arr = [uniform(a + 1e-3, b - 1e-3) for i in range(n_elements)]
     return x_arr
+
+def grad_check_sparse(f, x, analytic_grad, num_checks=10, h=1e-5):
+    """
+    sample a few random elements and only return numerical
+    in this dimensions.
+    """
+    for i in range(num_checks):
+        ix = tuple([randrange(m) for m in x.shape])
+
+        oldval = x[ix]
+        x[ix] = oldval + h # increment by h
+        fxph = f(x) # evaluate f(x + h)
+        x[ix] = oldval - h # increment by h
+        fxmh = f(x) # evaluate f(x - h)
+        x[ix] = oldval # reset
+
+        grad_numerical = ((fxph - fxmh) / (2 * h))[ix]
+        grad_analytic = analytic_grad[ix]
+        rel_error = (abs(grad_numerical - grad_analytic) /
+                    (abs(grad_numerical) + abs(grad_analytic)))
+        print(grad_numerical)
+        print(grad_analytic)
+        print(rel_error)
+        assert_almost_equal(rel_error, 0, decimal=5)
 
 @pytest.mark.parametrize(
     "method, y_d, domain",
@@ -106,7 +129,7 @@ def generate_test_data(n_elements=10, a=None, b=None):
         (np.reciprocal, lambda x: -1 / pow(x, 2), (None, 0)),
         (np.sin, lambda x: cos(x), None),
         (np.cos, lambda x: -sin(x), None),
-        (np.tan, lambda x: 1 / cos(x) ** 2, None),
+        (np.tan, lambda x: 1 / cos(x) ** 2, (-5, 5)), # # Set bound to prevent numerical overflow
         (np.arcsin, lambda x: 1 / sqrt(1 - x ** 2), (-1, 1)),
         (np.arccos, lambda x: -1 / sqrt(1 - x ** 2), (-1, 1)),
         (np.arctan, lambda x: 1 / (1 + x ** 2), None),
@@ -147,7 +170,7 @@ def test_unary_function(backend, method, y_d, domain):
         (lambda x: (np.log(x ** 2)) ** (1 / 3), lambda x: 2 * (np.log(x ** 2)) ** (-2/3) / (3 * x), (1, None)),
         (lambda x: np.log((1 + x) / (1 - x)) / 4 - np.arctan(x) / 2, lambda x: x ** 2 / (1 - x ** 4), (-1, 1)),
         (lambda x: np.arctanh(3 * x ** 3 + x ** 2 + 1), lambda x: (9 * x ** 2 + 2 * x) / (1 - (3 * x ** 3 + x ** 2 + 1) ** 2), (0, None)),
-        (lambda x: np.sinh(np.cbrt(x)) + np.cosh(4 * x ** 3) , lambda x: np.cosh(np.cbrt(x)) / (3 * x ** (2/3)) + 12 * (x ** 2) * np.sinh(4 * x ** 3), (1/4, None)),
+        (lambda x: np.sinh(np.cbrt(x)) + np.cosh(4 * x ** 3) , lambda x: np.cosh(np.cbrt(x)) / (3 * x ** (2/3)) + 12 * (x ** 2) * np.sinh(4 * x ** 3), (1/4, 1)), # Set bound to prevent numerical overflow
         (lambda x: np.log(1 + x ** 2) / np.arctanh(x), lambda x: ((2 * x * np.arctanh(x) / (1 + x ** 2)) - (np.log(1 + x ** 2) / (1 - x ** 2))) / (np.arctanh(x)) ** 2, (0, 1))
     ],
 )
